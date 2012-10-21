dcc03764c69d9a1a6a03baf05d4a46fa80b9294f
BangL <henno.rickowski@googlemail.com>
2012-10-21 15:36:41 +0200

    GPLv3.0

--- de/bangl/wgpdf/listener/PlayerDamageListener.java
+++ de/bangl/wgpdf/listener/PlayerDamageListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 BangL <henno.rickowski@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.bangl.wgpdf.listener;
 
 import de.bangl.wgpdf.Utils;
@@ -9,7 +25,7 @@ import org.bukkit.event.entity.EntityDamageEvent;
 
 /**
  *
- * @author BangL
+ * @author BangL <henno.rickowski@googlemail.com>
  */
 public class PlayerDamageListener implements Listener {
     WGPlayerDamageFlagsPlugin plugin;
